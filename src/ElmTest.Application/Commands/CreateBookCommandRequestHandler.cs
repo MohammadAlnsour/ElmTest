c6f1ebe7144897c18bb3bfb5a059bd7a114552fe
mohamad alnsour <moh.alnsour@gmail.com>
2024-08-07 14:17:17 +0300

    add insert book

--- src/ElmTest.Application/Commands/CreateBookCommandRequestHandler.cs
+++ src/ElmTest.Application/Commands/CreateBookCommandRequestHandler.cs
@@ -3,6 +3,7 @@ using ElmTest.Application.Contracts.ApiResponse;
 using ElmTest.Application.Requests;
 using ElmTest.Domain.Entities;
 using ElmTest.Domain.Factories;
+using ElmTest.Infrastructure.Repositories;
 using FluentValidation;
 using MediatR;
 
@@ -12,18 +13,28 @@ namespace ElmTest.Application.Commands
     {
         private readonly IBookFactory _bookFactory;
         private readonly IMapper _mapper;
+        private readonly IBookRepository _bookRepository;
 
-        public CreateBookCommandRequestHandler(IBookFactory bookFactory, IMapper mapper)
+        public CreateBookCommandRequestHandler(IBookFactory bookFactory, IMapper mapper, IBookRepository bookRepository)
         {
             _bookFactory = bookFactory;
             _mapper = mapper;
+            _bookRepository = bookRepository;
         }
-        public Task<long> Handle(CreateBookRequest request, CancellationToken cancellationToken)
+        public async Task<long> Handle(CreateBookRequest request, CancellationToken cancellationToken)
         {
             var bookInfo = _mapper.Map<CreateBookRequest, BookInfo>(request);
             var book = _bookFactory.CreateBook(bookInfo.BookTitle, bookInfo.BookDescription, bookInfo.Author, bookInfo.PublishDate, bookInfo.CoverBase64);
 
-            return Task.FromResult(long.MinValue);
+            try
+            {
+                return await _bookRepository.Insert(book);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
