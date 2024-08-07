cfc4072ed63a931a87fc3dbd30dbaf208c8ce7c8
mohamad alnsour <moh.alnsour@gmail.com>
2024-08-07 13:43:35 +0300

    get books paged

--- src/ElmTest.Application/Queries/GetBooksQueryPaginationRequestHandler.cs
+++ src/ElmTest.Application/Queries/GetBooksQueryPaginationRequestHandler.cs
@@ -1,18 +1,30 @@
-﻿using ElmTest.Application.Contracts.DTOs;
+﻿using AutoMapper;
+using ElmTest.Application.Contracts.DTOs;
 using ElmTest.Application.Requests;
+using ElmTest.Domain.Entities;
+using ElmTest.Domain.Factories;
+using ElmTest.Infrastructure.Repositories;
 using MediatR;
 
 namespace ElmTest.Application.Queries
 {
     public class GetBooksQueryPaginationRequestHandler : IRequestHandler<GetBooksPaginationRequest, IEnumerable<BookReponseDTO>>
     {
-        public GetBooksQueryPaginationRequestHandler()
+        private readonly IBookFactory _bookFactory;
+        private readonly IMapper _mapper;
+        private readonly IBookRepository _bookRepository;
+
+        public GetBooksQueryPaginationRequestHandler(IBookFactory bookFactory, IMapper mapper, IBookRepository bookRepository)
         {
-            
+            _bookFactory = bookFactory;
+            _mapper = mapper;
+            _bookRepository = bookRepository;
         }
-        public Task<IEnumerable<BookReponseDTO>> Handle(GetBooksPaginationRequest request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<BookReponseDTO>> Handle(GetBooksPaginationRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var dbBooks = await _bookRepository.GetPaged(request.PageNumber, request.PageSize);
+            var booksDTOs = _mapper.Map<IEnumerable<BookReponseDTO>>(dbBooks);
+            return booksDTOs;
         }
     }
 }
